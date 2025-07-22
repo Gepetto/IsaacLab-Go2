c9cd45f7d7aeddf118ff89d3760deaed458eeaae
Krzysztof Wojciechowski <krzy.wojciecho@gmail.com>
2025-07-22 23:40:31 +0200

    Rename files

--- source/go2_locomotion/go2_locomotion/tasks/locomotion/velocity/config/anymal_d/rough_env_cfg.py
+++ source/go2_locomotion/go2_locomotion/tasks/locomotion/velocity/config/anymal_d/rough_env_cfg.py
@@ -1,6 +1,6 @@
 from isaaclab.utils import configclass
 
-from ext_template.tasks.locomotion.velocity.velocity_env_cfg import LocomotionVelocityRoughEnvCfg
+from go2_locomotion.tasks.locomotion.velocity.velocity_env_cfg import LocomotionVelocityRoughEnvCfg
 
 ##
 # Pre-defined configs

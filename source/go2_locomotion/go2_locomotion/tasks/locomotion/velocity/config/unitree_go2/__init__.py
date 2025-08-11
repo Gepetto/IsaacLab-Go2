7a5e70ad6379fb96050e854983f76c5f00e87e60
KABOURI Mohamed Yassine <kabouri.yassine@hotmail.com>
2025-08-11 17:35:41 +0200

    Add CaT and clean_rl (should be registered for default env)

--- source/go2_locomotion/go2_locomotion/tasks/locomotion/velocity/config/unitree_go2/__init__.py
+++ source/go2_locomotion/go2_locomotion/tasks/locomotion/velocity/config/unitree_go2/__init__.py
@@ -2,6 +2,10 @@ import gymnasium as gym
 
 from . import agents, flat_env_cfg, rough_env_cfg
 
+from go2_locomotion.tasks.mdps.cat.cat_env import CaTEnv
+from go2_locomotion.tasks.locomotion.velocity.cat_velocity_env_cfg import (
+    Go2FlatEnvCfg, Go2FlatEnvCfg_PLAY
+)
 ##
 # Register Gym environments.
 ##
@@ -43,5 +47,25 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": rough_env_cfg.UnitreeGo2RoughEnvCfg_PLAY,
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:UnitreeGo2RoughPPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="Isaac-Velocity-CaT-Flat-Go2-v0",
+    entry_point=CaTEnv,
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.cat_flat_env_cfg:Go2FlatEnvCfg",
+        "clean_rl_cfg_entry_point": f"{agents.__name__}.clean_rl_ppo_cfg:Go2FlatPPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="Isaac-Velocity-CaT-Flat-Go2-Play-v0",
+    entry_point=CaTEnv,
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.cat_flat_env_cfg:Go2FlatEnvCfg_PLAY",
+        "clean_rl_cfg_entry_point": f"{agents.__name__}.clean_rl_ppo_cfg:Go2FlatPPORunnerCfg",
     },
 )

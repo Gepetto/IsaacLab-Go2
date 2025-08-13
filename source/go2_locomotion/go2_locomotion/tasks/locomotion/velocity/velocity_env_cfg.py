1367545cd51c6a0529c69e40c58993af8f8d1be9
Krzysztof Wojciechowski <krzy.wojciecho@gmail.com>
2025-08-13 11:12:06 +0200

    Add comment for no base velocity

--- source/go2_locomotion/go2_locomotion/tasks/locomotion/velocity/velocity_env_cfg.py
+++ source/go2_locomotion/go2_locomotion/tasks/locomotion/velocity/velocity_env_cfg.py
@@ -140,6 +140,7 @@ class ObservationsCfg:
         """Observations for policy group."""
 
         # observation terms (order preserved)
+        # For deployment purposes base velocity is disabled, as we do not have any estimator
         # base_lin_vel = ObsTerm(func=mdp.base_lin_vel, noise=Unoise(n_min=-0.1, n_max=0.1))
         base_ang_vel = ObsTerm(func=mdp.base_ang_vel, noise=Unoise(n_min=-0.2, n_max=0.2))
         projected_gravity = ObsTerm(

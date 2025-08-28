a7e426b155851b3254c0fddeb1ef7b5927bd66b6
Krzysztof Wojciechowski <krzy.wojciecho@gmail.com>
2025-08-28 16:17:29 +0200

    Change slurm task name

--- slurm/base_job.sh
+++ slurm/base_job.sh
@@ -6,7 +6,7 @@
 #SBATCH --constraint="rtx_a6000|nvidia_rtx_6000_ada_generation"  # Select specific GPUs
 #SBATCH --cpus-per-task=10                                       # 10 CPU cores per task
 #SBATCH --hint=nomultithread                                     # Physical cores only
-#SBATCH --time=5:00:00                                           # Maximum execution time (HH:MM:SS). Maximum 20h
+#SBATCH --time=0:00:20                                           # Maximum execution time (HH:MM:SS). Maximum 20h
 #SBATCH --output=logs/out/%x_%A_%a.out                           # Output log
 #SBATCH --error=logs/err/%x_%A_%a.err                            # Error log
 
@@ -16,7 +16,7 @@ source ${ALL_CCFRWORK}/issac_lab/bin/activate
 # Run training
 set -x
 python scripts/reinforcement_learning/cleanrl/train.py \
-    --task=Isaac-Velocity-Flat-H1-v0 \
+    --task=Go2-Velocity-Flat \
     --headless \
     --num_envs=4096 \
     --logger=wandb
